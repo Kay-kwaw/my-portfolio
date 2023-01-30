2af40e0e3b240987b5bc2fdb279ff3e7164a99df
kwaw-kumi <kwawkumi9@gmail.com>
2023-01-29 21:58:20 -0800

    css style

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const nav = document.querySelector('#nav');
 
-window.addEventListener ('scroll', () => {
-    console.log("scrolled");
-})
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+}
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
